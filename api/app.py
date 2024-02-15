01cb90d13e852b94d553932cab14b0d78deb6e4f
2am2 <17.amar1@gmail.com>
2024-02-14 17:37:28 -0800

    putting in contingency for too small liked playlis

--- api/app.py
+++ api/app.py
@@ -56,6 +56,7 @@ def get_all_tracks():
     playlist_length = 100
     # iter = playlist_length//50
     count = 0
+    return sp.current_user_saved_tracks.__sizeof__()
     while count*50 < 100:
         tracklist += sp.current_user_saved_tracks(limit = 50, offset = count*50)["items"]
         count += 1
