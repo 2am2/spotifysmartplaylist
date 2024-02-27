78d6ee11a136267913351cb630d36bec9db5c29f
2am2 <17.amar1@gmail.com>
2024-02-26 16:49:38 -0800

    indexing json wrong, lets check!

--- api/app.py
+++ api/app.py
@@ -86,7 +86,7 @@ def setPlaylist():
         playlist_set.add(playlists["items"][idx]["name"])
 
         #problems after this
-        return list((playlist_set))
+        return playlists["items"]
     
     if playlist_name in playlist_set:
         playlist = playlists["items"][idx]
