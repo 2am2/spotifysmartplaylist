bee40d82b936441deb25068093d60bfbe41949af
2am2 <17.amar1@gmail.com>
2024-02-26 17:24:00 -0800

    ???

--- api/app.py
+++ api/app.py
@@ -86,7 +86,7 @@ def setPlaylist():
         playlist_set.add(playlists["items"][idx]["name"])
 
         #problems after this
-        return playlists["items"][1]
+        return list(playlist_set)
     
     if playlist_name in playlist_set:
         playlist = playlists["items"][idx]
